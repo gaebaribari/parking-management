8d5b9e3a702207ba5c643d3c72e30fdd620be005
gaebaribari <gaebaribari@gmail.com>
2025-02-03 20:42:08 +0900

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="bg-red-100 p-4 rounded-lg shadow m-4 min-h-screen">
+    <span className="text-blue-500 font-bold text-xl">tailwind test</span>
+</div>
   );
 }
 
