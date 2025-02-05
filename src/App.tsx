36afcd99ecff88ea47af52bd717037248dc5d13d
gaebaribari <gaebaribari@gmail.com>
2025-02-05 16:27:04 +0900

    feat: payment.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import Login from './pages/login';
+import Settlement from './pages/settlement';
+import Payment from './pages/payment';
 
 export default function App() {
   return (
     <div className=' h-screen flex flex-col items-center justify-center p-5'>
       <h1>주차 정산 서비스</h1>
-      <Login></Login>
+      {/* <Login></Login> */}
+      {/* <Settlement></Settlement> */}
+      <Payment></Payment>
     </div>
   );
 }
