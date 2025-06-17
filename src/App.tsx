59e11a20ad9292fe9482a4b548b3a4f4339c0aad
gaebaribari <gaebaribari@gmail.com>
2025-06-17 11:05:28 +0900

    fix: 배포 후 페이지 안 나오는 문제 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,28 @@ import Index from "./pages";
 import Payment from "./pages/payment/payment";
 import Success from "./pages/payment/success";
 import Error from "./pages/error";
+import { basename } from "path/win32";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Index />,
+			errorElement: <Error />,
+		},
+		{
+			path: "/payment",
+			element: <Payment />,
+		},
+		{
+			path: "/success",
+			element: <Success />,
+		},
+	],
 	{
-		path: "/",
-		element: <Index />,
-		errorElement: <Error />,
-	},
-	{
-		path: "/payment",
-		element: <Payment />,
-	},
-	{
-		path: "/success",
-		element: <Success />,
-	},
-]);
+		basename: "/parking-management",
+	}
+);
 
 export default function App() {
 	return <RouterProvider router={router} />;
